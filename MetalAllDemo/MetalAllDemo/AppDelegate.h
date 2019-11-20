7dde2a87c60090420d0b3c492d29f10987d17fc5
陈江林 <1215999200@qq.com>
2019-11-20 22:23:03 +0800

    新建应用

---
+++ MetalAllDemo/MetalAllDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetalAllDemo
+//
+//  Created by 陈江林 on 2019/11/20.
+//  Copyright © 2019 陈江林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetalAllDemo/MetalAllDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetalAllDemo
+//
+//  Created by 陈江林 on 2019/11/20.
+//  Copyright © 2019 陈江林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
